bdecfeed8e281cd7c77db68fd03bcf88357344a3
Harsh Chaplot <harshchaplot11@gmail.com>
2022-01-22 12:58:24 +0530

    Update 12. Reverse String.cpp

--- 12.  Reverse String.cpp
+++ 12.  Reverse String.cpp
@@ -28,3 +28,23 @@ public:
     }
 };
 
+/*
+Java Solution
+Same Approach as above
+*/
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while ( l<r ) {
+            swap(s, l, r);
+            l++;
+            r--;
+        }
+    }
+    public void swap( char[] s, int i, int j ) {
+        char tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+}
